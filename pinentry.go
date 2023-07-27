ebc50ff3bd96fdc03f838414b9ab3fb374d34944
Morten Linderud <morten@linderud.pw>
2023-07-28 00:04:23 +0200

    Added pinentry
    Signed-off-by: Morten Linderud <morten@linderud.pw>

---
+++ pinentry.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"errors"
+
+	"github.com/twpayne/go-pinentry"
+)
+
+var (
+	ErrPinentryCancelled = errors.New("cancelled pinentry")
+)
+
+func GetPinentry() ([]byte, error) {
+	// TODO: Include some additional key metadata
+	client, err := pinentry.NewClient(
+		pinentry.WithBinaryNameFromGnuPGAgentConf(),
+		pinentry.WithDesc("Enter PIN for TPM key"),
+		pinentry.WithGPGTTY(),
+		pinentry.WithPrompt("PIN:"),
+		pinentry.WithTitle("tpm-ssh-agent PIN entry"),
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer client.Close()
+
+	switch pin, fromCache, err := client.GetPIN(); {
+	case pinentry.IsCancelled(err):
+		return nil, ErrPinentryCancelled
+	case err != nil:
+		return nil, err
+	case fromCache:
+		return []byte(pin), nil
+	default:
+		return []byte(pin), nil
+	}
+}
