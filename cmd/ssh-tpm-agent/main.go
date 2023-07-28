2d20f93370b1b94b4b662200f4a352e5d44ec5d9
Morten Linderud <morten@linderud.pw>
2023-07-29 00:13:33 +0200

    utils: move GetTPM to utils
    Signed-off-by: Morten Linderud <morten@linderud.pw>

--- cmd/ssh-tpm-agent/main.go
+++ cmd/ssh-tpm-agent/main.go
@@ -1,42 +1,18 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"fmt"
 	"log"
 	"os"
-	"path"
 
 	"github.com/foxboron/ssh-tpm-agent/agent"
 	"github.com/foxboron/ssh-tpm-agent/key"
 	"github.com/foxboron/ssh-tpm-agent/pinentry"
-	swtpm "github.com/foxboron/swtpm_test"
+	"github.com/foxboron/ssh-tpm-agent/utils"
 	"github.com/google/go-tpm/tpm2/transport"
 	"golang.org/x/crypto/ssh"
 )
-
-var (
-	swtpmPath = "/var/tmp/ssh-tpm-agent"
-)
-
-// Smaller wrapper for getting the correct TPM instance
-func getTPM(f bool) (transport.TPMCloser, error) {
-	var tpm transport.TPMCloser
-	var err error
-	if f || os.Getenv("SSH_TPM_AGENT_SWTPM") != "" {
-		if _, err := os.Stat(swtpmPath); errors.Is(err, os.ErrNotExist) {
-			os.MkdirTemp(path.Dir(swtpmPath), path.Base(swtpmPath))
-		}
-		tpm, err = swtpm.OpenSwtpm(swtpmPath)
-	} else {
-		tpm, err = transport.OpenTPM()
-	}
-	if err != nil {
-		return nil, err
-	}
-	return tpm, nil
-}
 
 func main() {
 	flag.Usage = func() {
@@ -54,7 +30,7 @@ func main() {
 	}
 
 	if *setupFlag {
-		tpm, err := getTPM(*swtpmFlag)
+		tpm, err := utils.GetTPM(*swtpmFlag)
 		if err != nil {
 			log.Fatal(err)
 		}
--- utils/tpm.go
+++ utils/tpm.go
@@ -1,6 +1,11 @@
 package utils
 
 import (
+	"errors"
+	"os"
+	"path"
+
+	swtpm "github.com/foxboron/swtpm_test"
 	"github.com/google/go-tpm/tpm2"
 	"github.com/google/go-tpm/tpm2/transport"
 )
@@ -15,4 +20,26 @@ type handle interface {
 func FlushHandle(tpm transport.TPM, h handle) {
 	flushSrk := tpm2.FlushContext{FlushHandle: h}
 	flushSrk.Execute(tpm)
+}
+
+var (
+	swtpmPath = "/var/tmp/ssh-tpm-agent"
+)
+
+// Smaller wrapper for getting the correct TPM instance
+func GetTPM(f bool) (transport.TPMCloser, error) {
+	var tpm transport.TPMCloser
+	var err error
+	if f || os.Getenv("SSH_TPM_AGENT_SWTPM") != "" {
+		if _, err := os.Stat(swtpmPath); errors.Is(err, os.ErrNotExist) {
+			os.MkdirTemp(path.Dir(swtpmPath), path.Base(swtpmPath))
+		}
+		tpm, err = swtpm.OpenSwtpm(swtpmPath)
+	} else {
+		tpm, err = transport.OpenTPM()
+	}
+	if err != nil {
+		return nil, err
+	}
+	return tpm, nil
 }
