7b2682307a11bdad87b9a5b27a1469b5670bd10a
Morten Linderud <morten@linderud.pw>
2023-08-12 01:24:03 +0200

    Implement ssh-tpm-add
    Signed-off-by: Morten Linderud <morten@linderud.pw>

---
+++ cmd/ssh-tpm-add/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"github.com/foxboron/ssh-tpm-agent/agent"
+	sshagent "golang.org/x/crypto/ssh/agent"
+)
+
+var Version string
+
+const usage = `Usage:
+    ssh-tpm-add [FILE]
+
+Add a sealed TPM key to ssh-tpm-agent.
+
+Example:
+    $ ssh-tpm-add id_rsa.tpm`
+
+func main() {
+	flag.Usage = func() {
+		fmt.Println(usage)
+	}
+
+	fmt.Println(os.Args)
+	if len(os.Args) == 1 {
+		fmt.Println(usage)
+	}
+
+	socket := os.Getenv("SSH_AUTH_SOCK")
+	if socket == "" {
+		fmt.Println("Can't find any ssh-tpm-agent socket.")
+		os.Exit(1)
+	}
+
+	conn, err := net.Dial("unix", socket)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	path := os.Args[1]
+
+	b, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := sshagent.NewClient(conn)
+
+	_, err = client.Extension(agent.SSH_TPM_AGENT_ADD, b)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Identity added: %s", path)
+}
